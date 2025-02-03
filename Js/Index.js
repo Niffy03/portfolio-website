ff7902b0835bc58cd6a96fd13d5d7f22e7e43e9c
Niffy03 <nifesussana@gmail.com>
2025-02-03 01:03:34 +0100

    Initial commit

---
+++ Js/Index.js
@@ -0,0 +1,34 @@
+/*
+
+$(window).on('scroll',function() 
+{
+  var scrollTop = $(window).scrollTop();
+  if (scrollTop >= 100) {
+    $('body').addClass(fixed-header)
+  } else {
+    $('body').removeClass('fixed-header')
+  }
+})
+
+$(document).ready(function() {
+  new Typed('$type-it', {
+    strings: ['Designer','Developer','Freelancer'],
+    typeSpeed: 100,
+    loop:true
+  });*/
+
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+    }
+})
